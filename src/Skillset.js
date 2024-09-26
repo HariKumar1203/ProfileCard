333f50a4070eed1e2f4d45eb0493087667dd5246
Hari <harikumar123942@gmail.com>
2024-09-26 10:51:58 +0530

    initial commit

--- src/Skillset.js
+++ src/Skillset.js
@@ -2,7 +2,7 @@ import Skills from "./Skills"
 
 function Skillset({skills}){
     return(
-      <div className='skill-card-1' color='blue'>
+      <div className='skill-card-1'>
         {
           skills.map((items)=>(
             <Skills skill={items.skill} level={items.level} color={items.color}/>
